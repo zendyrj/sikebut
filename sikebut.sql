cdb8d34f380a40e47b87b544a4151d3e8298eb08
zendyrj <142783200+zendyrj@users.noreply.github.com>
2024-04-29 11:00:37 +0700

    Update sikebut.sql

--- sikebut.sql
+++ sikebut.sql
@@ -11,7 +11,7 @@
  Target Server Version : 100427 (10.4.27-MariaDB)
  File Encoding         : 65001
 
- Date: 25/04/2024 14:51:27
+ Date: 29/04/2024 11:00:32
 */
 
 SET NAMES utf8mb4;
@@ -36,6 +36,58 @@ INSERT INTO `agamas` VALUES (3, 'Katholik');
 INSERT INTO `agamas` VALUES (4, 'Hindu');
 INSERT INTO `agamas` VALUES (5, 'Budha');
 INSERT INTO `agamas` VALUES (6, 'Konghucu');
+
+-- ----------------------------
+-- Table structure for berkas
+-- ----------------------------
+DROP TABLE IF EXISTS `berkas`;
+CREATE TABLE `berkas`  (
+  `berkas_id` int NOT NULL AUTO_INCREMENT,
+  `berkas_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`berkas_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 39 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of berkas
+-- ----------------------------
+INSERT INTO `berkas` VALUES (1, 'SK CPNS');
+INSERT INTO `berkas` VALUES (2, 'SK PNS');
+INSERT INTO `berkas` VALUES (3, 'Ijasah Terakhir');
+INSERT INTO `berkas` VALUES (4, 'Transkrip Nilai Terakhir');
+INSERT INTO `berkas` VALUES (5, 'SKP');
+INSERT INTO `berkas` VALUES (6, 'Kartu Keluarga');
+INSERT INTO `berkas` VALUES (7, 'Kenaikan Gaji Berkala Terakhir');
+INSERT INTO `berkas` VALUES (8, 'SK Kenaikan Pangkat Terakhir');
+INSERT INTO `berkas` VALUES (9, 'Akta Nikah');
+INSERT INTO `berkas` VALUES (10, 'Akta Anak');
+INSERT INTO `berkas` VALUES (11, 'SK Jabatan Terakhir');
+INSERT INTO `berkas` VALUES (12, 'SK Jabatan Fungsional Terakhir');
+INSERT INTO `berkas` VALUES (13, 'PAK');
+INSERT INTO `berkas` VALUES (14, 'Daftar Riwayat Jabatan');
+INSERT INTO `berkas` VALUES (15, 'Ijin Belajar');
+INSERT INTO `berkas` VALUES (16, 'STILUD');
+INSERT INTO `berkas` VALUES (17, 'Uraian Tugas');
+INSERT INTO `berkas` VALUES (18, 'Tugas Belajar');
+INSERT INTO `berkas` VALUES (19, 'SK PIM');
+INSERT INTO `berkas` VALUES (20, 'Data Perorangan Calon Penerima Pensiun (DPCP)');
+INSERT INTO `berkas` VALUES (21, 'Pas Photo 3 x 4');
+INSERT INTO `berkas` VALUES (22, 'Surat Pernyataan Tidak Pernah Dijatuhi Hukuman Disiplin Selama 1 Tahun');
+INSERT INTO `berkas` VALUES (23, 'SK Kematian dari suami/istri yang dibuat oleh Kelurahan/Desa/Kecamatan');
+INSERT INTO `berkas` VALUES (24, 'SK Peninjauan Masa Kerja');
+INSERT INTO `berkas` VALUES (25, 'Surat Keterangan');
+INSERT INTO `berkas` VALUES (26, 'Sertifikat Pendidik');
+INSERT INTO `berkas` VALUES (27, 'NUKP');
+INSERT INTO `berkas` VALUES (28, 'SK Mutasi');
+INSERT INTO `berkas` VALUES (29, 'Surat Permohonan Berhenti atas Permintaan Sendiri');
+INSERT INTO `berkas` VALUES (30, 'SK Pemberhentian PNS');
+INSERT INTO `berkas` VALUES (31, 'Kartu Istri');
+INSERT INTO `berkas` VALUES (32, 'Kartu Suami');
+INSERT INTO `berkas` VALUES (33, 'Kartu Pegawai');
+INSERT INTO `berkas` VALUES (34, 'Surat Tanda Tamat Pelatihan (Prajab, Diklatsar)');
+INSERT INTO `berkas` VALUES (35, 'Akta Cerai');
+INSERT INTO `berkas` VALUES (36, 'SK Hukuman Disiplin');
+INSERT INTO `berkas` VALUES (37, 'Dokumen Berita Acara Sumpah PNS');
+INSERT INTO `berkas` VALUES (38, 'Lainnya');
 
 -- ----------------------------
 -- Table structure for dasarkgb
@@ -120,12 +172,30 @@ CREATE TABLE `jabatans`  (
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`jabatan_id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 20 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of jabatans
 -- ----------------------------
-INSERT INTO `jabatans` VALUES (1, 'Kepala Dinas', NULL, NULL);
+INSERT INTO `jabatans` VALUES (1, 'Kepala Dinas ', NULL, NULL);
+INSERT INTO `jabatans` VALUES (2, 'Sekretaris Dinas', NULL, NULL);
+INSERT INTO `jabatans` VALUES (3, 'Kepala Sub Bagian Umum dan Kepegawaian', NULL, NULL);
+INSERT INTO `jabatans` VALUES (4, 'Penyusun Rencana Kebutuhan Sarana dan Prasarana', NULL, NULL);
+INSERT INTO `jabatans` VALUES (5, 'Pengelola Kepegawaian', NULL, NULL);
+INSERT INTO `jabatans` VALUES (6, 'Perencana Ahli Muda', NULL, NULL);
+INSERT INTO `jabatans` VALUES (7, 'Bendahara', NULL, NULL);
+INSERT INTO `jabatans` VALUES (8, 'Verifikator Keuangan', NULL, NULL);
+INSERT INTO `jabatans` VALUES (9, 'Penata Laporan Keuangan', NULL, NULL);
+INSERT INTO `jabatans` VALUES (10, 'Penelaah Teknis Kebijakan', NULL, NULL);
+INSERT INTO `jabatans` VALUES (11, 'Pengelola Permodalan dan Investasi', NULL, NULL);
+INSERT INTO `jabatans` VALUES (12, 'Analis Kebijakan Ahli Madya', NULL, NULL);
+INSERT INTO `jabatans` VALUES (13, 'Analis Kebijakan Ahli Muda', NULL, NULL);
+INSERT INTO `jabatans` VALUES (14, 'Analis Kebijakan Ahli Muda', NULL, NULL);
+INSERT INTO `jabatans` VALUES (15, 'Analis Dokumen Perizinan', NULL, NULL);
+INSERT INTO `jabatans` VALUES (16, 'Pengadministrasi Perizinan', NULL, NULL);
+INSERT INTO `jabatans` VALUES (17, 'Pengelola Data Administrasi dan Verifikasi', NULL, NULL);
+INSERT INTO `jabatans` VALUES (18, 'Pranata Komputer Terampil', NULL, NULL);
+INSERT INTO `jabatans` VALUES (19, 'Orientasi IPDN', NULL, NULL);
 
 -- ----------------------------
 -- Table structure for migrations
@@ -175,6 +245,7 @@ INSERT INTO `opds` VALUES (1, 'DPMPTSP', '2024-04-22 11:29:52', '2024-04-22 11:2
 DROP TABLE IF EXISTS `paperlesses`;
 CREATE TABLE `paperlesses`  (
   `paperless_id` bigint UNSIGNED NOT NULL AUTO_INCREMENT,
+  `berkas_id` int NULL DEFAULT NULL,
   `pegawai_id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `paperless_file` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `paperless_ket` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
@@ -186,11 +257,11 @@ CREATE TABLE `paperlesses`  (
 -- ----------------------------
 -- Records of paperlesses
 -- ----------------------------
-INSERT INTO `paperlesses` VALUES (1, '1', 'skp.pdf', 'SKP 2022', NULL, NULL);
-INSERT INTO `paperlesses` VALUES (2, '1', 'skp.pdf', 'SKP 2022', NULL, NULL);
-INSERT INTO `paperlesses` VALUES (3, '1', 'skp.pdf', 'SKP 2022', NULL, NULL);
-INSERT INTO `paperlesses` VALUES (4, '1', 'skp.pdf', 'KGB 2022', NULL, NULL);
-INSERT INTO `paperlesses` VALUES (5, '1', 'skp.pdf', 'SKP 2022', NULL, NULL);
+INSERT INTO `paperlesses` VALUES (1, 5, '1', 'skp.pdf', 'SKP 2022', NULL, NULL);
+INSERT INTO `paperlesses` VALUES (2, 5, '1', 'skp.pdf', 'SKP 2022', NULL, NULL);
+INSERT INTO `paperlesses` VALUES (3, 1, '1', 'skp.pdf', 'CPNS', NULL, NULL);
+INSERT INTO `paperlesses` VALUES (4, 2, '1', 'skp.pdf', 'PNS', NULL, NULL);
+INSERT INTO `paperlesses` VALUES (5, 5, '1', 'skp.pdf', 'SKP 2022', NULL, NULL);
 
 -- ----------------------------
 -- Table structure for password_resets
@@ -217,6 +288,7 @@ CREATE TABLE `pegawais`  (
   `jabatan_id` int NOT NULL,
   `golru_id` int NULL DEFAULT NULL,
   `pegawai_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `pegawai_nik` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
   `pegawai_nip` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `nomorhp` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
   `alamat_lengkap` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
@@ -228,15 +300,30 @@ CREATE TABLE `pegawais`  (
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`pegawai_id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 5 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 20 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of pegawais
 -- ----------------------------
-INSERT INTO `pegawais` VALUES (1, 1, 1, 15, 'Ir. QURATUL AINI, M.Si.', '196708111999012001', '3213', '3213', '3213', '31231', '2024-04-25', 1, NULL, NULL, NULL);
-INSERT INTO `pegawais` VALUES (2, 1, 1, 3, '123', '123', '123', '213', 'Laki-laki', NULL, NULL, 2, NULL, '2024-04-25 01:37:36', '2024-04-25 01:37:36');
-INSERT INTO `pegawais` VALUES (3, 1, 1, 6, '123', '123', '3123', '123123', 'Laki-laki', '312321213', '0000-00-00', 5, NULL, '2024-04-25 01:45:18', '2024-04-25 01:45:18');
-INSERT INTO `pegawais` VALUES (4, 1, 1, 3, '21312', '312312', '3213', '32', 'Laki-laki', '123123', '2024-04-25', 4, NULL, '2024-04-25 01:50:28', '2024-04-25 01:50:28');
+INSERT INTO `pegawais` VALUES (1, 1, 1, 15, 'Ir. QURATUL AINI, M.Si', '3512085108670002', '196708111999012001', '085257655908', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+INSERT INTO `pegawais` VALUES (2, 1, 2, 14, 'IDDHA ARUM BAWANA, S.STP, M.Si.', '3512060512750001', '197512051996021003', '082142361857', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+INSERT INTO `pegawais` VALUES (3, 1, 3, 11, 'MELLY AGUSTINA, S.H., M.H.', '3512084208780004', '197808022007012011', '082334205000', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+INSERT INTO `pegawais` VALUES (4, 1, 4, 9, 'EKO ANDY WIDARTO, S.T.', '3512060501780003', '197801052008011011', '08123197497', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+INSERT INTO `pegawais` VALUES (5, 1, 5, 8, 'BADRUR ROCHIM', '3512070504850004', '198504052006041011', '085257624386', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+INSERT INTO `pegawais` VALUES (6, 1, 6, 11, 'AGUS BUDIYANTO, S.H.', '3512081408850006', '198508142006041003', '085233114433', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+INSERT INTO `pegawais` VALUES (7, 1, 7, 9, 'NANANG WAHYUDI, S.E.', '3512071405830002', '198305142010011002', '082141379693', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+INSERT INTO `pegawais` VALUES (8, 1, 8, 8, 'FOMA WEDI KARTIKA', '3512061504770001', '197704152009031001', '085236536311', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+INSERT INTO `pegawais` VALUES (9, 1, 9, 9, 'NUR ULFA APRILIA, S.E.', '3512086004990001', '199904202022042001', '082340048300', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+INSERT INTO `pegawais` VALUES (10, 1, 10, 12, 'NURHASANAH, S.Sos.', '3512135012800002', '198012102007012010', '085313920763', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+INSERT INTO `pegawais` VALUES (11, 1, 11, 8, 'AMIR MAHFUD', '\"3512072402720001', '197202242007011006', '085204861614', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+INSERT INTO `pegawais` VALUES (12, 1, 12, 12, 'ZAINUL ARIFIN, S.P.', '\"3514132002790003', '197902202009031004', '082330719718', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+INSERT INTO `pegawais` VALUES (13, 1, 13, 12, 'SUWIGNYO AFANDI, S.Sos.', '3512081306710002', '197106131992111001', '08123485361', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+INSERT INTO `pegawais` VALUES (14, 1, 14, 12, 'SITI SANIYAH, S.Sos.', '3512074505660001', '196605051990032013', '081357356034', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+INSERT INTO `pegawais` VALUES (15, 1, 15, 9, 'RIZAL BAHRONI, S.Tr.IP.', '3512101108980001', '199808112022081001', '085258313202', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+INSERT INTO `pegawais` VALUES (16, 1, 16, 9, 'SONY NOVAYANTO, S.H.', '\"3512051711820001', '198211172010011001', '085336035463', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+INSERT INTO `pegawais` VALUES (17, 1, 17, 8, 'NURAHMAD', '3512050101730008', '197301012009031001', '082332609811', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+INSERT INTO `pegawais` VALUES (18, 1, 18, 7, 'ZENDY ROBI JUNIANTO, A.Md.Kom.', '3512072106950001', '199506212020121009', '085257500090', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+INSERT INTO `pegawais` VALUES (19, 1, 19, 9, 'BERLIANA EKA YORINDA, S.Tr.IP.', NULL, '199904272023082001', '', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
 
 -- ----------------------------
 -- Table structure for personal_access_tokens
@@ -425,13 +512,20 @@ DROP VIEW IF EXISTS `v_paperless`;
 CREATE ALGORITHM = UNDEFINED SQL SECURITY DEFINER VIEW `v_paperless` AS SELECT
 	paperlesses.*, 
 	pegawais.pegawai_nip, 
-	pegawais.pegawai_name
+	pegawais.pegawai_name, 
+	berkas.berkas_name
 FROM
 	paperlesses
-	INNER JOIN
+	LEFT JOIN
 	pegawais
 	ON 
-		paperlesses.pegawai_id = pegawais.pegawai_id ;
+		paperlesses.pegawai_id = pegawais.pegawai_id
+	LEFT JOIN
+	berkas
+	ON 
+		paperlesses.berkas_id = berkas.berkas_id
+ORDER BY
+	berkas.berkas_id ASC ;
 
 -- ----------------------------
 -- View structure for v_pegawai
