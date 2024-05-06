12a43720a4c998f7dac8d89f94c0f493eec89efc
zendyrj <142783200+zendyrj@users.noreply.github.com>
2024-05-06 16:03:43 +0700

    9

--- resources/views/pegawai/create.blade.php
+++ resources/views/pegawai/create.blade.php
@@ -2,7 +2,7 @@
     <div class="modal-dialog modal-xl">
       <div class="modal-content">
         <div class="modal-header">
-          <h4 class="modal-title">Persetujuan</h4>
+          <h4 class="modal-title">Form Pegawai</h4>
           <button type="button" class="close" data-dismiss="modal" aria-label="Close">
             <span aria-hidden="true">&times;</span>
           </button>
